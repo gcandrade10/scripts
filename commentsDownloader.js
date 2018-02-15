9f5b2766af245ca9ba7b538e866bf43a83273d8e
gcandrade10 <gc.andrade10@uniandes.edu.co>
2018-02-14 22:55:05 -0500

    Create commentsDownloader.js

---
+++ commentsDownloader.js
@@ -0,0 +1,23 @@
+var global =parseInt(document.getElementsByClassName("fcg")[0].textContent.split(" ")[0].replace(".",""));
+var coments =document.getElementsByClassName("UFICommentActorAndBody");
+function myFunction() 
+{
+    setTimeout(alertFunc, 3000);
+}
+function alertFunc() 
+{
+	var a =document.getElementsByClassName("UFIPagerLink")[0]; 
+	a.click();
+    coments =document.getElementsByClassName("UFICommentActorAndBody");
+    console.log(coments.length);
+    if(coments.length<global)
+    {
+        myFunction();
+    }
+    
+}
+alertFunc();
+for(i=0;i<coments.length;i++)
+{
+console.log(coments[i].innerText);
+}
